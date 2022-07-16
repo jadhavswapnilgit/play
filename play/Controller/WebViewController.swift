6a05add81023ba12191a15ac54da75b42bb4b7b2
swapnil <swapnilj1234@gmail.com>
2022-07-16 17:11:50 +0530

    webview config

---
+++ play/Controller/WebViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WebViewController.swift
+//  play
+//
+//  Created by Sachin Jadhav on 16/07/22.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController,WKNavigationDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+   
+
+}
