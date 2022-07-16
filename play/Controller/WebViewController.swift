5375fecc51945f225d801fa8617adf62925a1c1c
swapnil <swapnilj1234@gmail.com>
2022-07-16 17:13:03 +0530

    webview implement

--- play/Controller/WebViewController.swift
+++ play/Controller/WebViewController.swift
@@ -10,10 +10,29 @@ import WebKit
 
 class WebViewController: UIViewController,WKNavigationDelegate {
 
+    
+    var webView: WKWebView!
+    
+    var urls : String?
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+        guard let urlE = urls else {
+            return
+        }
+
+        let url = URL(string: urlE)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true        // Do any additional setup after loading the view.
     }
     
 
